309434d4dce0bbb70772402edc362f3df31fdd44
Douglas Henrique <contato@douglashsilva.com.br>
2020-03-17 01:13:35 -0300

    Atualizações

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,60 +1,60 @@
 class App {
 
     constructor(){
-        this.insertCertificates();
+        this.cdn = "http://cdn.douglashsilva.com.br"
+        this.appendCertificates()
     }
 
-    getCertificates(){
-        return new Promise((resolve,reject) => {
-            fetch("assets/data/certificates.json")
-            .then((data) => data.json())
-            .then((data) => {
-                const certificates = [];
-                for(let i = 0, length = data.length; i < length; i++){
-                    let certificate = null;
-                    const box = document.createElement("div"), img = document.createElement("img");
-                    img.setAttribute("src", "https://lh3.googleusercontent.com/" + data[i].id);
-                    img.setAttribute("alt", data[i].name);
-                    img.setAttribute("id", "certificate-" + data[i].number);
-                    box.appendChild(img);
-                    box.setAttribute("title", data[i].name);
-                    box.setAttribute("onclick", "app.openCertificate(this)");
-                    box.style.cursor = "pointer";
-                    certificate = box.outerHTML;
-                    certificates.push(certificate);
-                }
-                resolve(certificates.join(""));
-            }).catch((error) => console.error(error));
-        }); 
+    async getCertificates(){
+        const certificates = await fetch("assets/data/certificates.json")
+        return certificates.map((certificate) => {
+            return {
+                name: certificate.name,
+                src: `${this.cdn}/assets/certificates/${certificate.filename}`
+            }
+        })
+    }
+
+    async makeCertificates(){
+        const certificates = await this.getCertificates()
+        return certificates.map((certificate, index) => {
+            let box = document.createElement("div"), img = document.createElement("img")
+            img.setAttribute("src", certificate.src)
+            img.setAttribute("alt", certificate.name)
+            img.setAttribute("id", "certificate-00000" + index)
+            box.appendChild(img)
+            box.setAttribute("title", certificate.name)
+            box.setAttribute("onclick", "app.openCertificate(this)")
+            box.style.cursor = "pointer"
+            return box.outerHTML
+        }).join("")
     }
 
     openCertificate(certificate){
-        const body = document.createElement("body");
-        const img = document.createElement("img");
-        const dom = window.open("","_blank");
-        body.setAttribute("onselectstart","return false");
-        body.setAttribute("oncontextmenu","return false");
-        body.setAttribute("ondragstart","return false");
-        body.style.overflow = "hidden";
-        body.style.backgroundColor = "rgba(239, 249, 250, 0.6)";
-        body.style.display = "flex";
-        body.style.justifyContent = "center";
-        body.style.alignItems = "center";
-        img.style.maxWidth = window.innerWidth >= 600 ? "70%" : "90%";
-        img.src = certificate.querySelector("img").getAttribute("src");
-        body.appendChild(img);
-        dom.document.write(body.outerHTML);
-        dom.document.close();  
+        const body = document.createElement("body")
+        const img = document.createElement("img")
+        const dom = window.open("","_blank")
+        body.setAttribute("onselectstart","return false")
+        body.setAttribute("oncontextmenu","return false")
+        body.setAttribute("ondragstart","return false")
+        body.style.overflow = "hidden"
+        body.style.backgroundColor = "rgba(239, 249, 250, 0.6)"
+        body.style.display = "flex"
+        body.style.justifyContent = "center"
+        body.style.alignItems = "center"
+        img.style.maxWidth = window.innerWidth >= 600 ? "70%" : "90%"
+        img.src = certificate.querySelector("img").getAttribute("src")
+        body.appendChild(img)
+        dom.document.write(body.outerHTML)
+        dom.document.close()  
     }
 
-    insertCertificates(){
-        this.getCertificates()
-        .then((data) => {
-            const content = document.querySelector("div#tab-content-certificates > section");
-            content.innerHTML = data;
-        });
+    async appendCertificates(){
+        const certificates = await this.makeCertificates()
+        const content = document.querySelector("div#tab-content-certificates > section")
+        content.innerHTML = certificates
     }
 
 }
 
-const app = new App();
+const app = new App()
